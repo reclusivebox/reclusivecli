a859e7af63890a4ba9cd7543ce071b930a493bfe
Bruno Gregório <bruno.gregorio.silva@outlook.com>
2019-11-01 18:16:07 -0300

    Now RCLI is language-agnostic! At least when we talk about the languages in which you write the specification.

--- rcli/__init__.py
+++ rcli/__init__.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-import re
-import sys
-import json
-import rclerrors
 from rcllib import resolve_subcommand
 
 name = "rcli"
@@ -15,21 +11,12 @@ name = "rcli"
 """
 
 
-def command_to_dict(rcli_spec, arglist):
-    spec = json.loads(rcli_spec)
+def parse_command(rcli_spec, arglist):
 
     main_command = {
         "subcommand": {}
     }
 
-    resolve_subcommand(spec, main_command, arglist, 0)
+    resolve_subcommand(rcli_spec, main_command, arglist, 0)
 
     return main_command["subcommand"]
-
-
-# if __name__ == "__main__":
-#     with open("test.json", "r") as jsonfile:
-#         test_dict = json.loads(jsonfile.read())
-#
-#     for command in test_dict:
-#         print(command_to_dict(json.dumps(test_dict[command]), command.split(" ")))
